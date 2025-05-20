6070dc8a045cba4731df50d41d4fd04af5fca4d2
tatevar1 <97586693+tatevar1@users.noreply.github.com>
2025-05-20 17:09:29 +0400

    sign tests are fixed

--- cypress/e2e/SignIn/Signin.ts
+++ cypress/e2e/SignIn/Signin.ts
@@ -1,83 +1,98 @@
-import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-import generalTexts from '../../support/generalTexts'
-import SignInPage from '../../support/page-objects/SignInPage'
-import SignUpPage from '../../support/page-objects/SignUpPage'
+import generalTexts from "../../support/generalTexts";
+import SignInPage from "../../support/page-objects/SignInPage";
+import SignUpPage from "../../support/page-objects/SignUpPage";
 
-const signInPage = new SignInPage()
-const signUpPage = new SignUpPage()
+const signInPage = new SignInPage();
+const signUpPage = new SignUpPage();
 
 //Test-1
-Given('I open the the Sign in page', () => {
-    signInPage.navigate()
-})
-When('I input valid email address and valid password and click on the Sign in button', () => {
-    signInPage.signIn(generalTexts.mainEmail, generalTexts.mainPassword)
-})
-Then('I see the user signed in successfully', () => {
-    cy.checkAccessToken()
-})
+Given("I open the the Sign in page", () => {
+  signInPage.navigate();
+});
+When(
+  "I input valid email address and valid password and click on the Sign in button",
+  () => {
+    signInPage.signIn(generalTexts.mainEmail, generalTexts.mainPassword);
+  }
+);
+Then("I see the user signed in successfully", () => {
+  cy.checkAccessToken();
+});
 //Test-2
-When('I input valid Email address and invalid Password and click on the Sign in button', () => {
-    cy.loginWithErrorHandling(generalTexts.mainEmail, 'invalidPassword')
-})
+When(
+  "I input valid Email address and invalid Password and click on the Sign in button",
+  () => {
+    signInPage.signIn(generalTexts.mainEmail, "invalidPassword");
+  }
+);
 
-Then('I see the validation message displayed under the password field', () => {
-    cy.invalidCredentialsMsgIsDisplayed(generalTexts.invalidCredentialsMsg)
-})
+Then("I see the validation message displayed for password", () => {
+  cy.invalidCredentialsMsgIsDisplayed(generalTexts.invalidCredentialsMsg);
+});
 
 //Test-3
-When('I input invalid Email address and valid Password and click on the button Sign in', () => {
-    signInPage.signIn('tatevikscal.io', generalTexts.mainPassword)
-})
-Then('I see the user is not signed in and the validation message displayed under the email field', () => {
-    cy.invalidEmailMsgIsDisplayed(generalTexts.invalidEmailMsg)
-})
+When(
+  "I input invalid Email address and valid Password and click on the button Sign in",
+  () => {
+    signInPage.signIn("tatevikscrib.io", generalTexts.mainPassword);
+  }
+);
+Then(
+  "I see the user is not signed in and the validation message displayed for email",
+  () => {
+    cy.invalidEmailMsgIsDisplayed(generalTexts.invalidCredentialsMsg);
+  }
+);
 
 //Test-4
-When('I input valid Email address and leave the password field blank and click on the Sign in button', () => {
-    signInPage.enterEmail(generalTexts.mainEmail)
-    signInPage.clickSignInBtn()
-})
-Then('I see the user is not signed in and the validation message displayed under the password field', () => {
-    cy.invalidCredentialsMsgIsDisplayed(generalTexts.emptyPasswordMsg)
-})
+When(
+  "I input valid Email address and leave the password field blank and click on the Sign in button",
+  () => {
+    signInPage.enterEmail(generalTexts.mainEmail);
+    signInPage.clickSignInBtn();
+  }
+);
+Then(
+  "I see the user is not signed in and the validation message displayed for blank password",
+  () => {
+    cy.emptyFieldMsgIsDisplayed(generalTexts.emptyPasswordMsg);
+  }
+);
 
 //Test-5
-When('I input valid password and leave the Email field blank and click on the Sign in button', () => {
-    signInPage.enterPassword(generalTexts.mainPassword)
-    signInPage.clickSignInBtn()
-})
-Then('I see the validation message displayed under the email field', () => {
-    cy.invalidEmailMsgIsDisplayed(generalTexts.emptyEmailMsg)
-})
+When(
+  "I input valid password and leave the Email field blank and click on the Sign in button",
+  () => {
+    signInPage.enterPassword(generalTexts.mainPassword);
+    signInPage.clickSignInBtn();
+  }
+);
+Then("I see the validation message displayed for blank email", () => {
+  cy.emptyFieldMsgIsDisplayed(generalTexts.emptyEmailMsg);
+});
 
 //Test-6
-When('I leave the password and email fields blank and click on the Sign in button', () => {
-    signInPage.clickSignInBtn()
-})
+When(
+  "I leave the password and email fields blank and click on the Sign in button",
+  () => {
+    signInPage.clickSignInBtn();
+  }
+);
 Then(
-    'I see the user is not signed in and the validation messages displayed under the email and password fields',
-    () => {
-        cy.invalidEmailMsgIsDisplayed(generalTexts.emptyEmailMsg)
-        cy.invalidCredentialsMsgIsDisplayed(generalTexts.emptyPasswordMsg)
-    }
-)
+  "I see the user is not signed in and the credentials validation messages displayed",
+  () => {
+    cy.emptyFieldMsgIsDisplayed(generalTexts.emptyEmailMsg);
+    cy.emptyFieldMsgIsDisplayed(generalTexts.emptyPasswordMsg);
+  }
+);
 
 //Test-7
-When('I click on the show hide icon', () => {
-    signInPage.enterPassword(generalTexts.mainPassword)
-    signInPage.clickEyeIcon()
-})
-Then('I see the password shown or hidden in the password field', () => {
-    signInPage.checkPasswordEye()
-})
-
-//Test-8
-When('I click on the sign up button', () => {
-    signInPage.navigate()
-    signInPage.clickSignUpBtn()
-})
-Then('I see the user redirect to the sign up page', () => {
-    signUpPage.signUpPageOpened()
-})
+When("I click on the show hide icon", () => {
+  signInPage.enterPassword(generalTexts.mainPassword);
+  signInPage.clickEyeIcon();
+});
+Then("I see the password shown or hidden in the password field", () => {
+  signInPage.checkPasswordEye();
+});
